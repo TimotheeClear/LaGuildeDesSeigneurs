e9254b123e9ca753db6c45d7f75827e766265fcd
TimotheeClear <89906603+TimotheeClear@users.noreply.github.com>
2022-02-07 16:41:52 +0100

    Commit v0.5.1

--- tests/CharacterControllerTest.php
+++ tests/CharacterControllerTest.php
@@ -6,15 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class CharacterControllerTest extends WebTestCase
 {
+    private $client;
+
+    public function setup() : void
+    {
+        $this->client = static::createClient();
+    }
+
     /**
      * Tests index
      */
     public function testIndex()
     {
-        $client = static::createClient();
-        $client->request('GET', '/character/index');
+        $this->client->request('GET', '/character/index');
 
-        $response = $client->getResponse();
+        $response = $this->client->getResponse();
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertTrue($response->headers->contains('Content-Type', 'application/json'), $response->headers);
     }
@@ -24,17 +30,42 @@ class CharacterControllerTest extends WebTestCase
      */
     public function testDisplay()
     {
-        $client = static::createClient();
+        $client = $this->client;
         $client->request('GET', '/character/display/a9399b0bd1b3cfa35cd4d2b888070cb07096aab2');
 
         $this->assertJsonResponse($client->getResponse());
     }
 
     /**
+     * Tests redirect
+     */
+    public function testRedirectIndex()
+    {
+        $this->client->request('GET', '/character');
+        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * Tests bad identifier
+     */
+    public function testBadIdentifier(){
+        $this->client->request('GET', '/character/display/badIdentifier');
+        $this->assertError404($this->client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * Assert that Response returns 404
+     */
+    public function assertError404($statusCode){
+        $this->assertEquals(404, $statusCode);
+    }
+    
+    /**
      * Asserts that a Response is in json
      */
-    public function assertJsonResponse($response)
+    public function assertJsonResponse()
     {
+        $response = $this->client->getResponse();
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertTrue($response->headers->contains('Content-Type', 'application/json'), $response->headers);
     }
