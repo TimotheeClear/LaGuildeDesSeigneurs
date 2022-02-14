c618b46e5328c6b0abbce07796fd5165bcc6d8c2
TimotheeClear <89906603+TimotheeClear@users.noreply.github.com>
2022-02-14 13:04:09 +0100

    Commit v0.7.1

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Character;
+use DateTime;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 5; $i++) {
+            $character = new Character();
+            $character
+                ->setKind('Seigneur')
+                ->setName('Gorthol' . $i)
+                ->setSurname('Haume de terreur')
+                ->setCaste('Chevalier')
+                ->setKnowledge('Diplomatie')
+                ->setIntelligence(110)
+                ->setLife(11)
+                ->setImage('/images/gorthol.jpg')
+                ->setIdentifier(hash('sha1', uniqid()))
+                ->setCreation(new DateTime())
+            ;
+
+            $manager->persist($character);
+            $character = new Character();
+            $character
+                ->setKind('Dame')
+                ->setName('Eldalótë' . $i)
+                ->setSurname('Fleur elfique')
+                ->setCaste('Elfe')
+                ->setKnowledge('Arts')
+                ->setIntelligence(150)
+                ->setLife(10)
+                ->setImage('/images/eldalote.jpg')
+                ->setIdentifier(hash('sha1', uniqid()))
+                ->setCreation(new DateTime())
+            ;
+            $manager->persist($character);
+        }
+
+        $manager->flush();
+    }
+}
